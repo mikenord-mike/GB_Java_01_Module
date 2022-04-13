08d02f37701c11317287346c1565f028e49e6cf8
mikenord-mike <mikenord.s@gmail.com>
2022-04-13 22:39:58 +0300

    HW01 Task 06 final

--- src/main/java/ru/gb/mikenord/homework01/HomeWorkApp.java
+++ src/main/java/ru/gb/mikenord/homework01/HomeWorkApp.java
@@ -3,7 +3,10 @@ package ru.gb.mikenord.homework01;
 public class HomeWorkApp {
 
     public static void main(String [] args) {
-
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
     }
 
     private static void printThreeWords() {
