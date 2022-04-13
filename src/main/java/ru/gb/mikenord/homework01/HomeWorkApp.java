bb1a493198311d8e3320b1aa5071c97f6cd20f2b
mikenord-mike <mikenord.s@gmail.com>
2022-04-13 22:18:45 +0300

    HW01 Task 03

--- src/main/java/ru/gb/mikenord/homework01/HomeWorkApp.java
+++ src/main/java/ru/gb/mikenord/homework01/HomeWorkApp.java
@@ -11,4 +11,15 @@ public class HomeWorkApp {
         System.out.println("Banana");
         System.out.println("Apple");
     }
+
+    private static void checkSumSign() {
+        int a = 10, b = 20;
+        int c = a + b;
+
+        if (c >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
 }
