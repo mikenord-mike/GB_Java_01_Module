02f9bea54e66df27c7a242ad753facd2f58f2947
mikenord-mike <mikenord.s@gmail.com>
2022-04-13 22:15:49 +0300

    HW01 Task 02

--- src/main/java/ru/gb/mikenord/homework01/HomeWorkApp.java
+++ src/main/java/ru/gb/mikenord/homework01/HomeWorkApp.java
@@ -5,4 +5,10 @@ public class HomeWorkApp {
     public static void main(String [] args) {
 
     }
+
+    private static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
 }
