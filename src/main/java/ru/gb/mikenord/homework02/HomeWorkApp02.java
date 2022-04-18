d0fb9eb5123b64467260282f44df538c4006653e
mikenord-mike <mikenord.s@gmail.com>
2022-04-18 11:38:25 +0300

    HW02 Task 03

--- src/main/java/ru/gb/mikenord/homework02/HomeWorkApp02.java
+++ src/main/java/ru/gb/mikenord/homework02/HomeWorkApp02.java
@@ -4,7 +4,8 @@ public class HomeWorkApp02 {
 
     public static void main(String[] args) {
         System.out.println(sumCheck(5, 11));
-        signNumberCheck(-4);
+        signNumberPrint(-4);
+        System.out.println(negativeNumberCheck(0));
     }
 
     public static boolean sumCheck(int a, int b) {
@@ -12,11 +13,15 @@ public class HomeWorkApp02 {
         return sum >= 10 && sum <= 20;
     }
 
-    public static void signNumberCheck(int a) {
+    public static void signNumberPrint(int a) {
         if (a < 0) {
             System.out.println("The number is negative");
         } else {
             System.out.println("The number is positive");
         }
+    }
+
+    public static boolean negativeNumberCheck(int a) {
+        return a < 0;
     }
 }
