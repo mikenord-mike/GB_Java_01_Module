b424aad77a02bb6d5d4342b64985446abc0c75fd
mikenord-mike <mikenord.s@gmail.com>
2022-04-19 22:24:56 +0300

    HW03 Task 05

--- src/main/java/ru/gb/mikenord/homework03/HomeWorkApp03.java
+++ src/main/java/ru/gb/mikenord/homework03/HomeWorkApp03.java
@@ -10,14 +10,14 @@ public class HomeWorkApp03 {
         for (int i = 0; i < a.length; i++) {
             a[i] = a[i] == 0 ? 1 : 0;
         }
-        System.out.println(Arrays.toString(a));
+        System.out.println("t01 " + Arrays.toString(a));
 
         // task 02
         int[] b = new int[100];
         for (int i = 0; i < b.length; i++) {
             b[i] = i + 1;
         }
-        System.out.println(Arrays.toString(b));
+        System.out.println("t02 " + Arrays.toString(b));
 
         // task 03
         int[] c = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
@@ -26,16 +26,25 @@ public class HomeWorkApp03 {
                 c[i] *= 2;
             }
         }
-        System.out.println(Arrays.toString(c));
+        System.out.println("t03 " + Arrays.toString(c));
 
         // task 04
         int[][] d = new int[11][11];
         for (int i = 0; i < d.length; i++) {
             d[i][i] = 1;
             d[d.length - i - 1][i] = 1;
-
         }
-        System.out.println(Arrays.deepToString(d));
+        System.out.println("t04 " + Arrays.deepToString(d));
+
+        // task 05 check
+        System.out.println("t05 " + Arrays.toString(fillArray(90, 654)));
+
     }
 
+    // task 05
+    public static int[] fillArray(int len, int initialValue) {
+        int[] result = new int[len];
+        Arrays.fill(result, initialValue);
+        return result;
+    }
 }
