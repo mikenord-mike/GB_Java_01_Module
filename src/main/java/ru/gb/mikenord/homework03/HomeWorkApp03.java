ed3a358d1ac6cd46957fa4c5df73e90af0fcc916
mikenord-mike <mikenord.s@gmail.com>
2022-04-20 14:19:43 +0300

    HW03 Task 08 final

--- src/main/java/ru/gb/mikenord/homework03/HomeWorkApp03.java
+++ src/main/java/ru/gb/mikenord/homework03/HomeWorkApp03.java
@@ -5,21 +5,21 @@ import java.util.Arrays;
 public class HomeWorkApp03 {
     public static void main(String[] args) {
 
-        // task 01
+            // task 01
         int[] a = {0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1};
         for (int i = 0; i < a.length; i++) {
             a[i] = a[i] == 0 ? 1 : 0;
         }
         System.out.println("t01 " + Arrays.toString(a));
 
-        // task 02
+            // task 02
         int[] b = new int[100];
         for (int i = 0; i < b.length; i++) {
             b[i] = i + 1;
         }
         System.out.println("t02 " + Arrays.toString(b));
 
-        // task 03
+            // task 03
         int[] c = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
         for (int i = 0; i < c.length; i++) {
             if (c[i] < 6) {
@@ -28,7 +28,7 @@ public class HomeWorkApp03 {
         }
         System.out.println("t03 " + Arrays.toString(c));
 
-        // task 04
+            // task 04
         int[][] d = new int[11][11];
         for (int i = 0; i < d.length; i++) {
             d[i][i] = 1;
@@ -36,11 +36,11 @@ public class HomeWorkApp03 {
         }
         System.out.println("t04 " + Arrays.deepToString(d));
 
-        // task 05 check
+            // task 05 check
         System.out.println("t05 " + Arrays.toString(fillArray(90, 654)));
 
-        // task 06
-        // Not a very precise task condition - the array type is not specified. I solved it for integer type
+            // task 06
+            // Not a very precise task condition - the array type is not specified. I solved it for integer type
         int[] e = {5, 6, 9, 1, 456, -79, 4, 3, 2, 7};
         int min = 0, max = 0;
         for (int i = 0; i < e.length; i++) {
@@ -52,20 +52,26 @@ public class HomeWorkApp03 {
                 max = max < e[i] ? e[i] : max;
             }
         }
-        System.out.printf("min = %d, max = %d\n", min, max);
+        System.out.printf("t06 min = %d, max = %d\n", min, max);
 
-        // task 07 check
-        System.out.println(sumRightLeftArrayPartCheck(new int[]{-12, 1, 1, 2, -8}));
+            // task 07 check
+        System.out.println("t07 " + sumRightLeftArrayPartCheck(new int[]{-12, 1, 1, 2, -8}));
+
+            // task 08 check
+        int[] h = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        arrayShift(h, -2);
+        System.out.println("t08 " + Arrays.toString(h));
+
     }
 
-    // task 05
+        // task 05
     public static int[] fillArray(int len, int initialValue) {
         int[] result = new int[len];
         Arrays.fill(result, initialValue);
         return result;
     }
 
-    // task 07
+        // task 07
     public static boolean sumRightLeftArrayPartCheck(int[] f) {
         for (int i = 1; i < f.length; i++) {
             int sumLeft = 0, sumRight = 0;
@@ -80,5 +86,20 @@ public class HomeWorkApp03 {
             }
         }
         return false;
+    }
+
+        // task 08
+    public static void arrayShift(int[] g, int n) {
+        n %= g.length;
+        if (n < 0) {
+            n += g.length;
+        }
+        for (int i = 0; i < n; i++) {
+            int tmp = g[g.length - 1];
+            for (int j = g.length - 1; j > 0; j--) {
+                g[j] = g[j - 1];
+            }
+            g[0] = tmp;
+        }
     }
 }
