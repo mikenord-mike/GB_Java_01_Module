4510710d56e404da9163bcd1228f9f93a977f67e
mikenord-mike <mikenord.s@gmail.com>
2022-04-21 22:56:55 +0300

    HW02 Task 06 corrections of method names

--- src/main/java/ru/gb/mikenord/homework02/HomeWorkApp02.java
+++ src/main/java/ru/gb/mikenord/homework02/HomeWorkApp02.java
@@ -3,19 +3,26 @@ package ru.gb.mikenord.homework02;
 public class HomeWorkApp02 {
 
     public static void main(String[] args) {
-        System.out.println(sumCheck(5, 11));
-        signNumberPrint(-4);
-        System.out.println(negativeNumberCheck(0));
-        printSeveralTimes("GB Java Test", 3);
-        System.out.println(leapYearCheck(400));
+            // task 01 test
+        System.out.println(isSumInRange20To30(5, 11));
+            // task 02 test
+        printNumberSign(-4);
+            // task 03 test
+        System.out.println(isNegativeNumber(0));
+            // task 04 test
+        printStringSeveralTimes("GB Java Test", 3);
+            // task 05 test
+        System.out.println(isLeapYear(400));
     }
 
-    public static boolean sumCheck(int a, int b) {
+        // task 01
+    public static boolean isSumInRange20To30(int a, int b) {
         int sum = a + b;
         return sum >= 10 && sum <= 20;
     }
 
-    public static void signNumberPrint(int a) {
+        // task 02
+    public static void printNumberSign(int a) {
         if (a < 0) {
             System.out.println("The number is negative");
         } else {
@@ -23,17 +30,20 @@ public class HomeWorkApp02 {
         }
     }
 
-    public static boolean negativeNumberCheck(int a) {
+        // task 03
+    public static boolean isNegativeNumber(int a) {
         return a < 0;
     }
 
-    public static void printSeveralTimes(String s, int n) {
+        // task 04
+    public static void printStringSeveralTimes(String s, int n) {
         for (int i = 0; i < n; i++) {
             System.out.println(s);
         }
     }
 
-    public static boolean leapYearCheck(int year) {
+        // task 05
+    public static boolean isLeapYear(int year) {
         if (year % 4 == 0) {
             return !(year % 100 == 0 && year % 400 !=0);
         }
